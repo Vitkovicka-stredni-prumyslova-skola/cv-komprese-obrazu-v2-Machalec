272d5293f49374f485ee9d02532194f2da34b36e
Machalec <94050101+Machalec@users.noreply.github.com>
2024-03-21 13:03:39 +0100

    CV-pocet unikatnich barev

--- KompreseObrazu/Program.cs
+++ KompreseObrazu/Program.cs
@@ -1,29 +1,55 @@
-﻿namespace Komprese
+﻿using System;
+using System.Collections.Generic;
+
+namespace Komprese
 {
-    public class KompreseObrazu{
+    public class KompreseObrazu
+    {
+        public static void Main(string[] args)
+        {
+            // Cesta k testovacímu souboru
+            string testFilePath = @"C:\Users\MichalEliáš\github-classroom\Vitkovicka-stredni-prumyslova-skola\cv-komprese-obrazu-v2-Machalec\KompreseObrazu\CSV\obr1-10.csv";
 
-        public static void Main (String [] args){
-        
-        
+            // Vytvoření instance třídy Obrazek
+            Obrazek inputCSV = new Obrazek(testFilePath);
 
-        //Cesta k testovacímu souboru
-        String testFilePath = @"C:\Users\MartinKapcala\OneDrive - Vítkovická střední průmyslová škola\Dokumenty\Programování\PG\Komprese-obrazu\KompreseObrazu\CSV\obr1-10.csv";
-        
-        //vytvoření instance třídy Obrazek
-        Obrazek inputCSV = new Obrazek(testFilePath);
+            // Vytvoření 2D pole obsahujícího hodnoty barev obrázku
+            int[,] imageArray = inputCSV.GetImageArray();
 
-        
-        
-        //Test metody, která spočítá počet řádků vstupního obrázku
-        Console.WriteLine("Počet vertikálních řádků {0}",inputCSV.CountLines(testFilePath));
-        
-        //Test metody, která spočítá počet symbolů vstupního obrázku v prvním řádku
-        Console.WriteLine("Počet horizontální řádků {0}",inputCSV.CountSymbolInLine(testFilePath));
+            // Test metody, která spočítá počet řádků vstupního obrázku
+            Console.WriteLine("Počet vertikálních řádků {0}", inputCSV.CountLines(testFilePath));
 
-        inputCSV.vypisImg();
+            // Test metody, která spočítá počet symbolů vstupního obrázku v prvním řádku
+            Console.WriteLine("Počet horizontálních symbolů {0}", inputCSV.CountSymbolInLine(testFilePath));
+
+            // Výpis načteného obrázku
+            inputCSV.vypisImg();
+
+            // Seznam unikátních barev
+            List<int> uniqueColors = inputCSV.PaletaBarevObrazku();
+
+            // Vytvoření slovníku pro ukládání počtu výskytů jednotlivých barev v obrázku
+            Dictionary<int, int> colorCounts = new Dictionary<int, int>();
+
+            // Spočítat počty výskytů jednotlivých barev v obrázku
+            foreach (int color in uniqueColors)
+            {
+                int count = inputCSV.CountColorOccurrences(color);
+                colorCounts.Add(color, count);
+            }
+             
+            Console.WriteLine("---------------------------------");
+
+            Console.Write("Unikátní Barvy:");
+            foreach(int element in uniqueColors){
+                Console.Write($"{element}, ");
+            }
+            // Výpis jednotlivých barev a jejich počtu výskytů
+            Console.WriteLine("Barev v obrázku:");
+            foreach (var kvp in colorCounts)
+            {
+                Console.WriteLine($"Barva: {kvp.Key}, Počet výskytů: {kvp.Value}");
+            }
+        }
     }
-
-    }
-    
-    
 }
